2afcacc6c05edd1a2669646b58442f97b08f72c1
John-Dig <rohnfiggins@gmail.com>
2023-01-16 11:48:12 -0800

    finished Event-Handling-with-Event-Listeners-63of75

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,40 @@
-window.onload = function() {
-  // first we set up an event handler for the form submission
+window.addEventListener("load", function() {
+  
   let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    // then we print values to the story area;
-    // we're hardcoding these values for now
+  let resetBtn =document.getElementById("reset");
+  let story = document.getElementById("story");
+
+
+  form.addEventListener("submit", function() {
+   
+    reset.removeAttribute("class");
+  });
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    window.alert("Do you need a new computer? Visit www.superextracomputersales.com to find the best deals!");
+    //inputting answers into story
     document.querySelector("span#person1a").innerText = document.getElementById("person1Input").value;
     document.querySelector("span#person1b").innerText = document.getElementById("person1Input").value;
     document.querySelector("span#person1c").innerText = document.getElementById("person1Input").value;
-    document.querySelector("span#person2a").innerText = document.getElementById("person2Input").value;
+    document.querySelector("span#person2a").innerText = document.getElementById("person2Input").value
     document.querySelector("span#person2b").innerText = document.getElementById("person2Input").value;
     document.querySelector("span#animal").innerText = document.getElementById("animalInput").value;
     document.querySelector("span#animal").innerText = document.getElementById("animalInput").value;
     document.querySelector("span#verb").innerText = document.getElementById("verbInput").value;
     document.querySelector("span#noun").innerText = document.getElementById("nounInput").value;
     document.querySelector("span#exclamation").innerText = document.getElementById("exclamationInput").value;
-
-    // then we show the story by removing the class attribute
-    document.querySelector("div#story").removeAttribute("class");
-     // we prevent the default refresh action for the submit event
-     event.preventDefault();
-  };
-};
+    story.removeAttribute("class");
+    story.setAttribute("class","showing");
+  });
+  resetBtn.addEventListener("click", function(){
+    story.setAttribute("class", "hidden");
+    resetBtn.setAttribute("class", "hidden");
+    //reset form values
+    document.getElementById("person1Input").value= null;
+    document.getElementById("person2Input").value= null;
+    document.getElementById("animalInput").value= null;
+    document.getElementById("verbInput").value= null;
+    document.getElementById("nounInput").value= null;
+    document.getElementById("exclamationInput").value= null;
+  });
+});  
